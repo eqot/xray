3140138cd09a59e8fade5df76a8e7ac1ab2c418e
Ikuo Terado <eqobar@gmail.com>
2016-09-11 23:23:08 +0900

    Support Android classes

---
+++ sample/src/androidTest/java/com/eqot/xray/SampleAndroidTest.java
@@ -0,0 +1,36 @@
+package com.eqot.xray;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+@Xray(SampleAndroid.class)
+public class SampleAndroidTest {
+    private Context mContext;
+
+    @Before
+    public void setUp() throws Exception {
+        mContext = InstrumentationRegistry.getTargetContext();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void nop() throws Exception {
+        final SampleAndroid$Xray sample = new SampleAndroid$Xray();
+        assertEquals("123", sample.nop("123"));
+    }
+
+    @Test
+    public void nopWithContext() throws Exception {
+        final SampleAndroid$Xray sample = new SampleAndroid$Xray();
+        assertEquals("123", sample.nop(mContext, "123"));
+    }
+}
---
+++ sample/src/main/java/com/eqot/xray/SampleAndroid.java
@@ -0,0 +1,14 @@
+package com.eqot.xray;
+
+import android.content.Context;
+
+public class SampleAndroid {
+//    private final Context mContext;
+
+//    public SampleAndroid(Context context) {
+//        mContext = context;
+//    }
+
+    public String nop(String value) { return value; }
+    public String nop(Context context, String value) { return value; }
+}
