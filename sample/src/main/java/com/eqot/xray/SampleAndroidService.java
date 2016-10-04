b149a2852ac7cd91dc8aa65bb415f2f9f7084b57
Ikuo Terado <eqobar@gmail.com>
2016-10-05 03:15:20 +0900

    Add SampleAndroidService

---
+++ sample/src/main/java/com/eqot/xray/SampleAndroidService.java
@@ -0,0 +1,38 @@
+package com.eqot.xray;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+
+public class SampleAndroidService extends Service {
+    @SuppressWarnings("unused")
+    private static final String TAG = SampleAndroidService.class.getSimpleName();
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+}
+
